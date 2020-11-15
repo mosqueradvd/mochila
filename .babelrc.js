6c5244224e965672de31d4e851abbddaeffacc58
Cristian Cepeda <43882+pastuxso@users.noreply.github.com>
2020-11-14 19:43:02 -0500

    Agrega soporte para usa componentes de Material IU (#4)
    Cierra #1

---
+++ .babelrc.js
@@ -0,0 +1,45 @@
+const plugins = [
+  "@babel/plugin-proposal-class-properties",
+  [
+    "babel-plugin-import",
+    {
+      libraryName: "@material-ui/core",
+      libraryDirectory: "esm",
+      camel2DashComponentName: false,
+    },
+    "core",
+  ],
+  [
+    "babel-plugin-import",
+    {
+      libraryName: "@material-ui/icons",
+      libraryDirectory: "esm",
+      camel2DashComponentName: false,
+    },
+    "icons",
+  ],
+  [
+    "babel-plugin-import",
+    {
+      libraryName: "@material-ui/lab",
+      libraryDirectory: "esm",
+      camel2DashComponentName: false,
+    },
+    "lab",
+  ],
+];
+
+module.exports = {
+  "presets": [
+    [
+      "next/babel",
+      {
+        "preset-env": {},
+        "transform-runtime": {},
+        "styled-jsx": {},
+        "class-properties": {}
+      }
+    ]
+  ],
+  "plugins": plugins
+}
---
+++ next.config.js
@@ -0,0 +1,7 @@
+const withTM = require('next-transpile-modules')([
+  '@material-ui/core',
+  '@material-ui/labs',
+  '@material-ui/icons'
+]);
+
+module.exports = withTM()
