5ba947efad1768fbf473c9a62890de262f6ec97d
Lennin Geovanny Ibarra Gonzlez <42820375+lenninIbarrraGonzalez@users.noreply.github.com>
2021-01-06 19:07:20 -0500

    Dashboard admin (#89)
    * [Feature], implement add attached Files
    
    * se modifica el archivo de constantes para manejar los departamentos y ciudades
    
    * implementando dashboard admin

--- src/pages/admin/index.jsx
+++ src/pages/admin/index.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import {
+  Box,
+  Card,
   Container,
-  CssBaseline,
-  Typography,
-  Divider,
-  Box
+  CardContent,
+  Typography
 } from '@material-ui/core'
+import { useDispatch, useSelector } from 'react-redux'
 import { makeStyles } from '@material-ui/core/styles'
+import Grid from '@material-ui/core/Grid'
 import Layout from '@components/Layout'
+import { fetchOrganizations } from 'dux/organizationsSlice'
+import Skeleton from '@material-ui/lab/Skeleton'
 
 const useStyles = makeStyles((theme) => ({
   tipography: {
@@ -27,25 +31,67 @@ const useStyles = makeStyles((theme) => ({
 
 const ComponentDashboard = () => {
   const classes = useStyles()
-  return (
 
-    <Container className={classes.container}>
-      <Layout pageTitle='dashboard'>
-        <CssBaseline />
+  const dispatch = useDispatch()
+  const isLoading = useSelector(state => state.organizations.isLoading)
+  const organizations = useSelector(state => state.organizations.data)
+
+  useEffect(() => {
+    dispatch(fetchOrganizations())
+  }, [dispatch])
+
+  if (isLoading) {
+    return (
+      <div className={classes.skeleton}>
+        <Skeleton animation={false} width={350} height={30} />
+        <Skeleton animation='pulse' width={350} height={30} />
+        <Skeleton animation='pulse' width={350} height={30} />
+        <Skeleton animation='pulse' width={350} height={30} />
+        <Skeleton animation='pulse' width={350} height={30} />
+        <Skeleton animation='wave' width={350} height={30} />
+        <Skeleton animation='wave' width={350} height={30} />
+        <Skeleton animation='wave' width={350} height={30} />
+        <Skeleton animation='wave' variant='rect' width={350} height={200} />
+      </div>
+    )
+  }
+
+  return (
+    <Layout pageTitle='dashboard'>
+      <Container className={classes.container}>
         <Box className={classes.box}>
-          <Typography
-            variant='h4'
-            color='primary'
-            component='h1'
-            className={classes.tipography}
-          >
-            Tablero de comando
+          <Typography variant='h4' color='primary' component='h1'>
+            Organizaciones registradas
           </Typography>
         </Box>
-        <Divider />
-      </Layout>
-    </Container>
-
+        <div className={classes.root}>
+          <Grid container spacing={3}>
+            {organizations.map((row, index) => {
+              return (
+                <Grid item xs key={index}>
+                  <Card className={classes.card}>
+                    <CardContent>
+                      <Typography variant='h6' color='textSecondary' gutterBottom>
+                        {row?.name}
+                      </Typography>
+                      <Typography variant='h6' component='h2'>
+                        {row?.legalRepresentative}
+                      </Typography>
+                      <Typography variant='h6' component='h2'>
+                        {row?.projectType}
+                      </Typography>
+                      <Typography className={classes.pos} color='textSecondary'>
+                        {row?.community}
+                      </Typography>
+                    </CardContent>
+                  </Card>
+                </Grid>
+              )
+            })}
+          </Grid>
+        </div>
+      </Container>
+    </Layout>
   )
 }
 export default ComponentDashboard
