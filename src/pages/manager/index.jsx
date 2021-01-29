253ace6ee76915ae47e8caebe92463dc2783c972
Lennin Geovanny Ibarra Gonzlez <42820375+lenninIbarrraGonzalez@users.noreply.github.com>
2021-01-29 15:39:35 -0500

    Search (#187)
    * implementando un buscador haciendo uso de hook, useMemo, useRef
    
    * moviendo de hook a components search no es un hook es un componente de funcion

--- src/pages/manager/index.jsx
+++ src/pages/manager/index.jsx
@@ -11,7 +11,9 @@ import {
   CardActions,
   Button
 } from '@material-ui/core'
-import Search from '../../hooks/useSearch'
+
+import Search from 'components/Search'
+
 import Grid from '@material-ui/core/Grid'
 import { makeStyles } from '@material-ui/core/styles'
 import { fetchProjects } from 'dux/projectsSlice'
